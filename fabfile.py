277118add4de09390da839046070743d623a8357
Travis Hoppe <travis.hoppe@gmail.com>
2017-04-09 00:53:03 -0400

    Inital commit

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import local
+
+miniprez = "python ~/git-repo/miniprez/miniprez/miniprez.py"
+
+def run():
+    local("{} index.md".format(miniprez))
+
+def edit():
+    local("emacs index.md &")
+
+def watch():
+    local("{} --watch=1 index.md".format(miniprez))
+
+def view():
+    local("xdg-open index.html")
